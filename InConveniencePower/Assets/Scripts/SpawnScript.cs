448f54ae6aa6c4d3d52e83bc2f87af198d10f5c5
itc-c21026 <84373723+itc-c21026@users.noreply.github.com>
2022-05-30 16:35:59 +0900

    Update SpawnScript.cs

--- InConveniencePower/Assets/Scripts/SpawnScript.cs
+++ InConveniencePower/Assets/Scripts/SpawnScript.cs
@@ -6,9 +6,8 @@ public class SpawnScript : MonoBehaviour
 {
     public AudioClip SE1;
     AudioSource audioSource;
-    //public GameObject obj2;
 
-    //ƒXƒ|[ƒ“‚³‚¹‚éƒIƒuƒWƒFƒNƒg
+    //ã‚¹ãƒãƒ¼ãƒ³ã•ã›ã‚‹ã‚ªãƒ–ã‚¸ã‚§ã‚¯ãƒˆ
     public GameObject obj;
 
     public string Tag;
@@ -25,14 +24,10 @@ public class SpawnScript : MonoBehaviour
     public int i = 0;
 
     public int[] a;
-
-    //public MeshRenderer ms;
-
-    //public string[] TrFas;
+    
     // Start is called before the first frame update
     void Start()
     {
-        //ms = GetComponent<MeshRenderer>();
         audioSource = GetComponent<AudioSource>();
         spwanTime = 10;
         spwan = true;
@@ -42,11 +37,9 @@ public class SpawnScript : MonoBehaviour
 
     private void OnCollisionExit(Collision collision)
     {
-        //ƒIƒuƒWƒFƒNƒg‚ª—£‚ê‚é‚Æfalse‚É‚È‚é
+        //ã‚ªãƒ–ã‚¸ã‚§ã‚¯ãƒˆãŒé›¢ã‚Œã‚‹ã¨falseã«ãªã‚‹
         if (collision.gameObject.tag == Tag && spwanCnt == 0 && collision.gameObject.tag != "Ramen")
         {
-            //ms.enabled = true;
-            //Debug.Log(Tag + "!!!!!");
             Invoke("Spwanbool", spwanTime);
             spwanCnt++;
         }
@@ -56,16 +49,10 @@ public class SpawnScript : MonoBehaviour
     {
         if (RamenspwanCnt == 1)
         {
-            //Debug.Log("Ramen!!!!!");
             Invoke("Spwanbool", 1f);
             spwanCnt++;
             RamenspwanCnt++;
         }
-
-        /*if (b % 2 == 0)
-        {
-            Debug.Log()
-        }*/
 
         if (spwanCnt >= 1)
         {
@@ -77,48 +64,25 @@ public class SpawnScript : MonoBehaviour
             }
         }
         
-        //false‚É‚È‚é‚ÆƒIƒuƒWƒFƒNƒg‚ªoŒ»‚µ‚Ätrue‚É‚È‚é
+        //falseã«ãªã‚‹ã¨ã‚ªãƒ–ã‚¸ã‚§ã‚¯ãƒˆãŒå‡ºç¾ã—ã¦trueã«ãªã‚‹
         if (spwan == false)
         {
             audioSource.PlayOneShot(SE1);
             GameObject Obj =  Instantiate(obj, new Vector3(this.transform.position.x, this.transform.position.y+3, this.transform.position.z), Quaternion.identity);
             
             Obj.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
-            //Debug.Log("spawn");
             spwan = true;
         }
     }
-
-    /*private void OnCollisionEnter(Collision collision)
-    {
-        obj2.SetActive(true);
-    }*/
 
     public void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Ramen")
         {
             a[i] = i;
-            //Debug.Log(a[i]);
-            //TrFas[i] = "true";
-            //Debug.Log(TrFas[i]);
-            //i++;
 
         }
     }
-
-    /*private void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.tag == Tag)
-        {
-        }
-        else
-        {
-            spwanCnt = 1;
-            b += Time.deltaTime;
-            Debug.Log(b);
-        }
-    }*/
 
     void Spwanbool()
     {
